1789957fe916096e9b7c580c09848a2534c00cf0
Dioser Tinoco Rodriguez <dioser.tinoco@gmail.com>
2025-07-27 23:17:46 -0500

    Ordenamos la clase Principal

--- src/com/aluracursos/conversordemoneda/Principal.java
+++ src/com/aluracursos/conversordemoneda/Principal.java
@@ -19,57 +19,19 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
 
+        //Declaramos variables principales
+        String apiKey = "ffc0cc3b4086110623160275";
+
+        //Interactuamos con el usuario
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese la moneda que desea convertir: ");
         String tipoMoneda = scanner.nextLine();
-        String apiKey = "ffc0cc3b4086110623160275";
 
-
-        //Realizamos la conexion
-        //Conexion conexion = new Conexion(apiKey,tipoMoneda);
-
-        //HttpClient client = conexion.getClient();
-        //HttpResponse<String> response = conexion.getRequest();
-
-        //Clase
-        //parseamos la respuesta con JSON
-        //JsonElement jsonElement = JsonParser.parseString(response.body());
-        //JsonObject jsonObject = jsonElement.getAsJsonObject();
-
+        //Enviamos parametro y (Recuperamos los datos)
         ObtenerDatos oDatos = new ObtenerDatos(apiKey,tipoMoneda);
-        //JsonObject jsonObject = obtenerDatos.ParseJsonObject();
 
+        //Mostramos resultados de Record
         oDatos.Resultado();
-
-
-/*
-        //Clase
-        //Extraemos base_code
-        String basecode = jsonObject.get("base_code").getAsString();
-
-        //Extraemos el Objeto consersion_rates
-        JsonObject conversionRates = jsonObject.getAsJsonObject("conversion_rates");
-
-        //Obtenemos la moneda
-        String monedaDestino = "PEN";
-        double tasaConversion = conversionRates.get(monedaDestino).getAsDouble();
-
-        //Respuesta
-        System.out.println("Base: " + tipoMoneda
-                + " Moneda conversion: "
-                + monedaDestino
-                + " Monto: "
-                + tasaConversion);
-
-        //Gson gson = new Gson();
-        //RespuestaApi respuesta = gson.fromJson(response.body(), RespuestaApi.class);
-
-        //Extraemos la tasa de conversion
-        //String monedaDestino = "PEN";
-        //double tasa = respuesta.conversion_rates.get(monedaDestino);
-
-
-*/
 
 
     }
